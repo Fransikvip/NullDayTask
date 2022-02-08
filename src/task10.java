9efa7ed6894035972ded1ecc3e0ec0de1c819ab5
Fransikvip <Frans_87@tut.by>
2022-02-08 10:23:29 +0300

    sout

--- src/task10.java
+++ src/task10.java
@@ -1,20 +1,39 @@
 package Tasks;
 
 public class task10{
-    public static void main(String[] args) {
-        int n = 4;
-        int[][] array = new int[n][n];
-        for (int i = 0; i < n; i++) {
 
-            for (int j = 0; j < n; j++) {
-                if (i % 2 == 0) {
-                    array[i][j] = j + 1;
-                } else {
-                    array[i][j] = n - j;
+        public static void main(String[] args) {
+
+            int n = 4;
+            System.out.println("Длина массива = " + n);
+
+            int[][] arr = fillMatrix(n);
+            outputArr(arr);
+
+        }
+
+
+        public static void outputArr(int[][] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    System.out.print(arr[i][j] + " ");
                 }
+                System.out.println();
             }
         }
-        System.out.println(array);
-    }
 
-}
+
+        public static int[][] fillMatrix(int n) {
+            int[][] arr = new int[n][n];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (i % 2 == 0) {
+                        arr[i][j] = j + 1;
+                    } else {
+                        arr[i][j] = n - j;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
